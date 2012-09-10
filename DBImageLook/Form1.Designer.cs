a5a51f81099b5d5215914a38d554ababfebdaec5
Tobi Lehman <tobi.lehman@gmail.com>
2012-09-10 15:08:35 -0700

    Add source code for DBImageLook

---
+++ DBImageLook/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace DBImageLook
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbRawData = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblRawData = new System.Windows.Forms.Label();
+            this.btnShowImage = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbRawData
+            // 
+            this.tbRawData.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbRawData.Location = new System.Drawing.Point(77, 12);
+            this.tbRawData.Name = "tbRawData";
+            this.tbRawData.Size = new System.Drawing.Size(331, 20);
+            this.tbRawData.TabIndex = 0;
+            this.tbRawData.TextChanged += new System.EventHandler(this.tbRawData_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 67);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 234);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblRawData
+            // 
+            this.lblRawData.AutoSize = true;
+            this.lblRawData.Location = new System.Drawing.Point(12, 15);
+            this.lblRawData.Name = "lblRawData";
+            this.lblRawData.Size = new System.Drawing.Size(61, 13);
+            this.lblRawData.TabIndex = 2;
+            this.lblRawData.Text = "Raw Data: ";
+            // 
+            // btnShowImage
+            // 
+            this.btnShowImage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnShowImage.Location = new System.Drawing.Point(333, 38);
+            this.btnShowImage.Name = "btnShowImage";
+            this.btnShowImage.Size = new System.Drawing.Size(75, 23);
+            this.btnShowImage.TabIndex = 5;
+            this.btnShowImage.Text = "Show Image";
+            this.btnShowImage.UseVisualStyleBackColor = true;
+            this.btnShowImage.Click += new System.EventHandler(this.btnShowImage_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(12, 43);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(82, 13);
+            this.linkLabel1.TabIndex = 6;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Clear Raw Data";
+            this.linkLabel1.Visible = false;
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 313);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.btnShowImage);
+            this.Controls.Add(this.lblRawData);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.tbRawData);
+            this.Name = "Form1";
+            this.Text = "Look at Images stuffed in a Database";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbRawData;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblRawData;
+        private System.Windows.Forms.Button btnShowImage;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ DBImageLook/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DBImageLook
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private byte[] HexStringToBytes(string hex)
+        {
+            byte[] data = new byte[hex.Length /2];
+            int j = 0;
+            for (int i = 0; i < hex.Length; i+=2)
+            {
+               data[j] = Convert.ToByte(hex.Substring(i, 2), 16);
+               ++j;
+            }
+            return data;
+        }
+
+        private void btnShowImage_Click(object sender, EventArgs e)
+        {
+            if(Regex.IsMatch(tbRawData.Text, "^(0x)?[a-f0-9]+$", RegexOptions.IgnoreCase)) { 
+                var hex = tbRawData.Text.Substring(2);
+                if (hex.Length % 2 == 1) hex = hex.Substring(0, hex.Length - 1);
+
+                Bitmap bmp = new Bitmap(new MemoryStream(HexStringToBytes(hex)));
+                pictureBox1.Image = bmp;
+            } else {
+                MessageBox.Show("Invalid Image Data, must be hexadecimal string, optionally prepended with 0x");
+            }
+        }
+
+        private void tbRawData_TextChanged(object sender, EventArgs e)
+        {
+            linkLabel1.Visible = (tbRawData.Text.Length > 0);
+            
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            tbRawData.Text = "";
+            linkLabel1.Visible = false;
+        }
+    }
+}
